ebbb70f45faff49ae3cff63d4d743cc8d2f731ae
SashaDemi <demidenkosasha1996@gmail.com>
2023-08-28 11:14:15 +0300

    Fourth commit

--- src/components/Header/HeadComp.js
+++ src/components/Header/HeadComp.js
@@ -1,19 +1,22 @@
 import './HeadComp.css'
 import myImg from '../img/olx-logo.jpg'
+
 const HeadComp = () => {
     return (
         <div className='main-head-box'>
             <div className='header-box'>
-                    <a href='#' className='sticker-head-box'>
+                <div className='sticker-head-box'>
+                    <a href='/#'>
                         <img className='head-img-olx' src={myImg} alt="no img"/>
                     </a>
+                </div>
                 <div className='header-content'>
-                    <a href='#' className='language-head-box'> Язык I </a>
-                    <a href='#' className='select-language'> Мова </a>
-                    <a href='#' className='message-head-box'> Сообщения </a>
-                    <a href='#' className='like-head-box'> Like </a>
-                    <a href='#' className='profile-head-box'> Ваш профиль </a>
-                    <a href='#' className='advertise-head-box'> Подать объявление </a>
+                    <a href='/#' className='language-head-box'> Язык I </a>
+                    <a href='/#' className='select-language'> Мова </a>
+                    <a href='/#' className='message-head-box'> Сообщения </a>
+                    <a href='/#' className='like-head-box'> Like </a>
+                    <a href='/#' className='profile-head-box'> Ваш профиль </a>
+                    <a href='/#' className='advertise-head-box'> Подать объявление </a>
                 </div>
             </div>
         </div>
