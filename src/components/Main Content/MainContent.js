f7db0c0cb5f499bb5cff3967bdacb5bad50f0ea0
SashaDemi <demidenkosasha1996@gmail.com>
2023-09-14 12:44:29 +0300

    8th (.map)

--- src/components/Main Content/MainContent.js
+++ src/components/Main Content/MainContent.js
@@ -1,24 +1,27 @@
 import './MainContent.css'
-import Help from "./Help";
-import BabyWorld from "./BabyWorld";
-import RealEstate from "./RealEstate";
-import Zapchasty from "./Zapchasty";
-import Work from "./Work";
-import Animals from "./Animals";
-import HouseAndGarden from "./HouseAndGarden";
-import Electronic from "./Electronic";
-import Business from "./Business";
-import Rent from "./Rent";
-import Fashion from "./Fashion";
-import Hobbies from "./Hobbies";
-import Free from "./Free";
-import Swap from "./Swap";
-import School from "./School";
-import ForVictory from "./ForVictory";
-import CarForWar from "./CarForWar";
-import Cars from "./Cars";
+import help from "../img/main-content/dopomoga.png"
+import babyWorld from "../img/main-content/detskiy-mir.png"
+import realEstate from "../img/main-content/nedvizhimost.png"
+import cars from "../img/main-content/transport.png"
+import spares from "../img/main-content/zapchasti-dlya-transporta.png"
+import work from "../img/main-content/rabota.png"
+import animals from "../img/main-content/zhivotnye.png"
+import houseAndGarden from "../img/main-content/dom-i-sad.png"
+import electronic from "../img/main-content/elektronika.png"
+import ContentElement from "./ContentElement";
 
 const MainContent = () => {
+    let mainContentDataTop = [
+        {img: help, backColor: 'rgb(255, 206, 50)', text: 'Взатмопомощь'},
+        {img: babyWorld, backColor: 'rgb(58, 119, 255)', text: 'Детский мир'},
+        {img: realEstate, backColor: 'rgb(35, 229, 219)', text: 'Недвижимость'},
+        {img: cars, backColor: 'rgb(255, 86, 54)', text: 'Авто'},
+        {img: spares, backColor: 'rgb(255, 246, 217)', text: 'Запчасти для транспорта'},
+        {img: work, backColor: 'rgb(206, 221, 255)', text: 'Работа'},
+        {img: animals, backColor: 'rgb(200, 248, 246)', text: 'Животные'},
+        {img: houseAndGarden, backColor: 'rgb(255, 214, 201)', text: 'Дом и сад'},
+        {img: electronic, backColor: 'rgb(255, 206, 50)', text: 'Електроника'},
+    ]
     return (
         <div>
             <div className='head-content-box'>
@@ -26,18 +29,10 @@ const MainContent = () => {
             </div>
             <div className='content-wrapper'>
                 <div>
-                    <Help/>
-                    <BabyWorld/>
-                    <RealEstate/>
-                    <Cars/>
-                    <Zapchasty/>
-                    <Work/>
-                    <Animals/>
-                    <HouseAndGarden/>
-                    <Electronic/>
+                    {mainContentDataTop.map(e => <ContentElement data={e}/>)}
                 </div>
                 <div>
-                    <Business/>
+                    {/*<Business/>
                     <Rent/>
                     <Fashion/>
                     <Hobbies/>
@@ -45,7 +40,7 @@ const MainContent = () => {
                     <Swap/>
                     <CarForWar/>
                     <School/>
-                    <ForVictory/>
+                    <ForVictory/>*/}
                 </div>
             </div>
         </div>
